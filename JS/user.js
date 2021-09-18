708654f4516fd9ed7fd0e2e45bb077d99bd395c0
조혜령 <chohyeryungcho@gmail.com>
2021-09-19 02:17:25 +0900

    javascript base 1: object, jquery, json into table

---
+++ JS/user.js
@@ -0,0 +1,128 @@
+var user = [
+    {
+      "_id": "61461d851071530a110afbee",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Burris Murray",
+      "gender": "male",
+      "email": "burrismurray@xinware.com",
+      "phone": "+1 (849) 505-2368"
+    },
+    {
+      "_id": "61461d85ed4595d25b2b9681",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Felicia Carey",
+      "gender": "female",
+      "email": "feliciacarey@xinware.com",
+      "phone": "+1 (815) 543-2283"
+    },
+    {
+      "_id": "61461d8501b8ef57e0225587",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Celia Sharp",
+      "gender": "female",
+      "email": "celiasharp@xinware.com",
+      "phone": "+1 (830) 417-3182"
+    },
+    {
+      "_id": "61461d857fe62715fb054097",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Emma Olsen",
+      "gender": "female",
+      "email": "emmaolsen@xinware.com",
+      "phone": "+1 (814) 486-2556"
+    },
+    {
+      "_id": "61461d856d0dbd284ab0c80d",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Eve Becker",
+      "gender": "female",
+      "email": "evebecker@xinware.com",
+      "phone": "+1 (818) 482-3837"
+    },
+    {
+      "_id": "61461d85c114286ae267536f",
+      "age": 37,
+      "eyeColor": "blue",
+      "name": "Tracey Justice",
+      "gender": "female",
+      "email": "traceyjustice@xinware.com",
+      "phone": "+1 (823) 413-3640"
+    },
+    {
+      "_id": "61461d85a4ad3d12c64a48f1",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Lauren Snow",
+      "gender": "female",
+      "email": "laurensnow@xinware.com",
+      "phone": "+1 (988) 409-3299"
+    },
+    {
+      "_id": "61461d85ccf0ef98b72d4116",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Powers Odonnell",
+      "gender": "male",
+      "email": "powersodonnell@xinware.com",
+      "phone": "+1 (815) 420-3896"
+    },
+    {
+      "_id": "61461d85f47a81970be44dd6",
+      "age": 36,
+      "eyeColor": "brown",
+      "name": "Theresa Blackburn",
+      "gender": "female",
+      "email": "theresablackburn@xinware.com",
+      "phone": "+1 (936) 539-2002"
+    },
+    {
+      "_id": "61461d85c590d7e9ff67ec1f",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Jenny Powell",
+      "gender": "female",
+      "email": "jennypowell@xinware.com",
+      "phone": "+1 (896) 536-2594"
+    },
+    {
+      "_id": "61461d859fe086dbebe2db10",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Thomas Sloan",
+      "gender": "male",
+      "email": "thomassloan@xinware.com",
+      "phone": "+1 (933) 488-2141"
+    },
+    {
+      "_id": "61461d85b5d2f5e563d8c61b",
+      "age": 32,
+      "eyeColor": "brown",
+      "name": "Adams Dale",
+      "gender": "male",
+      "email": "adamsdale@xinware.com",
+      "phone": "+1 (890) 579-3931"
+    },
+    {
+      "_id": "61461d85292ec99105ffa71c",
+      "age": 35,
+      "eyeColor": "blue",
+      "name": "Rasmussen Bolton",
+      "gender": "male",
+      "email": "rasmussenbolton@xinware.com",
+      "phone": "+1 (970) 436-3301"
+    },
+    {
+      "_id": "61461d85d8df0513c8111881",
+      "age": 35,
+      "eyeColor": "blue",
+      "name": "Michael Everett",
+      "gender": "female",
+      "email": "michaeleverett@xinware.com",
+      "phone": "+1 (803) 446-3200"
+    }
+  ]
